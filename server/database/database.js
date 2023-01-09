5f7c8e9d2d65e7de8f942e52ebac532cb3b88675
amar kumar <amarkumar.sharma.124@gmail.com>
2023-01-09 10:14:09 +0530

    .env file configured

--- server/database/database.js
+++ server/database/database.js
@@ -1,10 +1,20 @@
+/*		*****Enviornment Variable*****		 */
+import dotenv from 'dotenv'
+dotenv.config();
+const USERNAME = process.env.DB_USERNAME;
+const PASSWORD = process.env.DB_PASSWORD;
+
+
+
+/*		*****Database Connection*****   	 */
 import mongoose from "mongoose";
+
 mongoose.set('strictQuery', false)
 var options = {
 	useNewUrlParser: true, useUnifiedTopology: true
 };
 const Connection = async()=>{
-	const MONGODB_URL = "mongodb+srv://mern-todo-app:mern-todo-app@mern-to-do.oro2wus.mongodb.net/?retryWrites=true&w=majority";
+	const MONGODB_URL = `mongodb+srv://${USERNAME}:${PASSWORD}@mern-to-do.oro2wus.mongodb.net/?retryWrites=true&w=majority`;
 	mongoose.connect(MONGODB_URL, options);
 	
 	mongoose.connection.on('connected', ()=>{
