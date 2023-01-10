9ac1d889e78b0225ae4b045754d14789c9887f9d
amar kumar <amarkumar.sharma.124@gmail.com>
2023-01-10 13:31:38 +0530

    bud fixed

--- client/src/redux/reducers/todosReducer.js
+++ client/src/redux/reducers/todosReducer.js
@@ -1,16 +1,21 @@
 import * as dispatchActionType from '../actions/dispatchType'
 
-export const todosReducers = (state=[], action) => {
+export const todosReducers = (state = [], action) => {
 	switch (action.type) {
+
 		case dispatchActionType.Add_New_Todo:
 			return [action.payload, ...state];
+
 		case dispatchActionType.Get_All_Todo:
 			return action.payload
+
 		case dispatchActionType.Toggle_Todo:
 			// console.log("toggeled data done ------>",action.payload.done);
 			return state.map(todo => (
-				todo._id === action.payload._id ? {...todo, done: !todo._id } : todo
+				todo._id === action.payload._id ? { ...todo, done: !todo.done } : todo
 			))
+
+		
 
 
 		default:
